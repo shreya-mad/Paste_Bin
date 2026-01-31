e1021fd53ed1c2eeaaba1d805bf3b4b9e60d0a54
Shreya Madeshiya <smadeshiya12345@gmail.com>
2026-02-01 00:22:15 +0530

    port chnage

--- server/Index.js
+++ server/Index.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use("/api/healthz", healthRoute);
 app.use("/api/pastes", pasteRoutes);
 
-const PORT =5001;
+const PORT =process.env.PORT || 5000;
+// console.log("🚀 Server starting on port "+PORT);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
